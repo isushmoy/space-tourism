02ec61ded4de4204d60564ab2782c81a8b7800d1
imsushmoy <hiraku.kazuma5@gmail.com>
2024-06-10 22:53:22 +0600

    add animation

--- src/components/animations.js
+++ src/components/animations.js
@@ -1,40 +1,40 @@
-// import gsap from 'gsap';
+import gsap from 'gsap';
 
-// // Function to check reduced motion preference
-// const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+// Function to check reduced motion preference
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
-// const animateContent = () => {
-//     if (!prefersReducedMotion) {
-//         gsap.fromTo(
-//             ".appear",
-//             { opacity: 0.25, y: 20 },
-//             { opacity: 1, y: 0, duration: 1 }
-//         );
-//     } else {
-//         // Reduced motion version
-//         gsap.fromTo(
-//             ".appear",
-//             { opacity: 0 },
-//             { opacity: 1, duration: 0.5 }
-//         );
-//     }
-// };
+const animateContent = () => {
+    if (!prefersReducedMotion) {
+        gsap.fromTo(
+            ".appear",
+            { opacity: 0.25, y: 20 },
+            { opacity: 1, y: 0, duration: 1 }
+        );
+    } else {
+        // Reduced motion version
+        gsap.fromTo(
+            ".appear",
+            { opacity: 0 },
+            { opacity: 1, duration: 0.5 }
+        );
+    }
+};
 
-// const onLoading = () => {
-//     if (!prefersReducedMotion) {
-//         gsap.fromTo(
-//             ".onLoad",
-//             { opacity: 0.25, y: 20 },
-//             { opacity: 1, y: 0, duration: 1 }
-//         );
-//     } else {
-//         // Reduced motion version
-//         gsap.fromTo(
-//             ".onLoad",
-//             { opacity: 0 },
-//             { opacity: 1, duration: 0.5 }
-//         );
-//     }
-// };
+const onLoading = () => {
+    if (!prefersReducedMotion) {
+        gsap.fromTo(
+            ".onLoad",
+            { opacity: 0.25, y: 20 },
+            { opacity: 1, y: 0, duration: 1 }
+        );
+    } else {
+        // Reduced motion version
+        gsap.fromTo(
+            ".onLoad",
+            { opacity: 0 },
+            { opacity: 1, duration: 0.5 }
+        );
+    }
+};
 
-// export { animateContent as default, onLoading };
+export { animateContent as default, onLoading };
